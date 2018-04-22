33181f9d8bf7f3e150bdbb7d32eae665d9e2a6b3
sevoster <sevoster@gmail.com>
2018-04-22 21:41:02 +0300

    Added test files in NetworkX format (json-graph).

--- core/decision_system.py
+++ core/decision_system.py
@@ -1,4 +1,8 @@
+from core.knowledge_base import KnowledgeBase
+
+
 class DecisionSystem:
     def __init__(self):
+        self.knowledge_base = KnowledgeBase()
         pass
 
--- core/knowledge_base.py
+++ core/knowledge_base.py
@@ -5,5 +5,11 @@ class KnowledgeBase:
 
     def __init__(self):
         # TODO: move hardcode to config
-        self.db_client = MongoClient('192.168.31.100', 27017)
+        self.client = MongoClient('192.168.31.100', 27017)
+        self.db = self.client.test_db
+        self.rules = self.db.rules
+        pass
+
+    def add_new_rules(self, rules):
+        self.rules.insert_many(rules)
         pass
